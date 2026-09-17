10e648f9c85ed791b80063fa68d979ebfdc7b6e5
xDivyanshx <135411867+xDivyanshx@users.noreply.github.com>
2026-09-18 00:47:41 +0530

    Time: 1 ms (75.8%), Space: 55.6 MB (62.2%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -7,6 +7,9 @@ public class Solution
         int ptr1 = 0, ptr2 = 0;
         int index = 0;
 
+
+        int pointToStop = totalArray.Length / 2;
+
         while (ptr1 < nums1.Length && ptr2 < nums2.Length)
         {
             if (nums1[ptr1] < nums2[ptr2])
@@ -17,16 +20,22 @@ public class Solution
             {
                 totalArray[index++] = nums2[ptr2++];
             }
+            if (index > pointToStop)
+                break;
         }
 
         while (ptr1 < nums1.Length)
         {
             totalArray[index++] = nums1[ptr1++];
+            if (index > pointToStop)
+                break;
         }
 
         while (ptr2 < nums2.Length)
         {
             totalArray[index++] = nums2[ptr2++];
+            if (index > pointToStop)
+                break;
         }
 
         if (totalArray.Length % 2 == 0)
