d6fa7ab50ba0a8eaeee8e309d0bab2227fdb100c
xDivyanshx <135411867+xDivyanshx@users.noreply.github.com>
2026-09-18 00:44:38 +0530

    Time: 2 ms (63.8%), Space: 55.3 MB (84.4%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -1,31 +1,43 @@
-// 4. Median of Two Sorted Arrays
-// Difficulty: Hard
-// https://leetcode.com/problems/median-of-two-sorted-arrays/
-// Runtime: 1 ms | Memory: 55.9 MB | Submitted: 2025-09-13
+public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] totalArray = new int[nums1.Length + nums2.Length];
 
-public class Solution {
-    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.Length, n = nums2.Length;
-        int[] merged = new int[m + n];
-        int i = 0, j = 0, k = 0;
+        int ptr1 = 0, ptr2 = 0;
+        int index = 0;
 
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                merged[k++] = nums1[i++];
-            } else {
-                merged[k++] = nums2[j++];
+        while (ptr1 < nums1.Length && ptr2 < nums2.Length)
+        {
+            if (nums1[ptr1] < nums2[ptr2])
+            {
+                totalArray[index++] = nums1[ptr1++];
+            }
+            else
+            {
+                totalArray[index++] = nums2[ptr2++];
             }
         }
-        while (i < m) merged[k++] = nums1[i++];
-        while (j < n) merged[k++] = nums2[j++];
 
-        int totalLength = m + n;
-        if (totalLength % 2 == 1) {
-            return merged[totalLength / 2];
-        } else {
-            int mid1 = merged[totalLength / 2 - 1];
-            int mid2 = merged[totalLength / 2];
-            return (mid1 + mid2) / 2.0;
+        while (ptr1 < nums1.Length)
+        {
+            totalArray[index++] = nums1[ptr1++];
+        }
+
+        while (ptr2 < nums2.Length)
+        {
+            totalArray[index++] = nums2[ptr2++];
+        }
+
+        if (totalArray.Length % 2 == 0)
+        {
+            int low = totalArray[(totalArray.Length - 1) / 2];
+            int high = totalArray[(totalArray.Length) / 2];
+            return (double)(low + high) / 2;
+        }
+        else
+        {
+            return totalArray[(totalArray.Length - 1) / 2];
         }
     }
 }
